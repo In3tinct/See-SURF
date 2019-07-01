65e3702fdad94ddb4ffd167deba5f7c05e48252b
In3tinct <vaibhav.agrawal0289@gmail.com>
2019-07-01 13:51:44 -0700

    Fixing validation check

--- see-surf.py
+++ see-surf.py
@@ -61,9 +61,7 @@ cookiesDict={}
 if args.cookies:
 	for cook in args.cookies:
                 cookiesDict[cook[:cook.find("=")]]=cook[cook.find("=")+1:]
-		#cookiesDict[cook.split("=")[0]]=cook.split("=")[1]
 
-print (cookiesDict)
 #Making an external request to a hostname through the potential vulnerable parameter to validate SSRF
 def makingExternalRequests(paramName, url):
 	regexToReplace=paramName+"=(.*?)(?:&|$)"
